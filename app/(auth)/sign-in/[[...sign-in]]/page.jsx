38062f8e4dc9403ebb11bb4af2c4cedaaffa3ef4
Khushi <khushi@gmail.com>
2025-05-23 23:20:23 +0530

    enhance ui of sign up

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -5,22 +5,16 @@ export default function Page() {
   return (
     <div className="min-h-screen flex">
     {/* Left Side - Image */}
-    <div className="hidden md:block md:w-1/2 lg:w-3/5 relative">
-      <Image src='/knowledge.png' width={1000} height={1000}  />
+    <div className="hidden md:flex md:w-1/2 lg:w-3/5 items-center justify-center relative">
+      <Image src='/LOGO.png' width={600} height={600}  />
       {/* Optional overlay */}
       <div className="absolute inset-0 bg-black/20"></div>
-      
-      {/* Optional text overlay */}
-      <div className="absolute bottom-10 left-10 text-white z-10">
-        <h2 className="text-4xl font-bold mb-2">KnowVal</h2>
-        <p className="text-xl">MiileStone 1 </p>
-      </div>
     </div>
 
     {/* Right Side - SignIn Form */}
     <div className="w-full md:w-1/2 lg:w-2/5 flex items-center justify-center p-8">
       <div className="w-full max-w-md">
-        <SignIn /> {/* Your existing component */}
+        <SignIn className="w-full h-full" /> {/* Your existing component */}
       </div>
     </div>
   </div>
