b359983047df238e0f6314f7fe1de442e526e5af
Khushi <khushi@gmail.com>
2025-05-25 19:12:24 +0530

    localhost:3000/dashboard

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -19,14 +19,14 @@ const Hero = () => {
       <div className="mt-4 flex justify-center gap-4 sm:mt-6">
         <a
           className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
-          href="/dashboard"
+          href="http://localhost:3000/dashboard"
         >
           Get Started
         </a>
 
         <a
           className="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900"
-          href="/dashboard"
+          href="http://localhost:3000/dashboard"
         >
           Learn More
         </a>
